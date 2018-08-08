8f7e42d2d8d2dd22dc58fd3c54174a66065e4f2a
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-07 22:25:51 -0300

    Criacao das rotas

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,6 +5,14 @@ var ctrlMain = require('../controllers/main');
 
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
+/* Location pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;

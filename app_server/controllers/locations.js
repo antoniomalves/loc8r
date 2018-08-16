c26ffad8655a53c0422eb8356fc7669d1e31c1bf
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-15 22:01:42 -0300

    retirando os dados da view location-list e levando para a controller.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,12 @@
 /* 'homelist' page */
 module.exports.homelist = function(req, res){
-  res.render('locations-list', {title: 'Home'});
+  res.render('locations-list', {
+    title: 'loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'loc8r',
+      strapline: 'Find places to work with near you!'
+    }
+  });
 };
 
 /* 'location Info' page */

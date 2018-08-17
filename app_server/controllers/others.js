ce01c4e40cd4c4c9847039389534d0e0f12dd1a0
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-16 22:28:54 -0300

    retirando os dados da visao sobre e passando para a controller.

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* 'about' page */
 module.exports.about = function(req, res){
-  res.render('generic-text', {title: 'About'});
+  res.render('generic-text', {
+    title: 'About Loc8r',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consecteturaccumsan vitae mauris eu, venenatis cursus ex. Integer at mauris non mi congue congue. Maecenas mi nulla, suscipit non nunc quis, facilisis viverra sapien. Etiam rutrum pretium mi, ut vehicula lorem tincidunt non. Nunc consectetur ipsum blandit odio sollicitudin, eget aliquam turpis interdum. Vivamus et mattis dolor. Curabitur imperdiet vestibulum tortor vitae dignissim. Aliquam ut placerat nulla. Vestibulum quis tempus quam. Donec interdum, nulla a laoreet finibus, arcu est cursus turpis, sit amet pulvinar metus neque sed massa. Maecenas ut enim sit amet augue rutrum viverra. Praesent in est mollis, maximus dui eget, lacinia lacus. Nullam vitae ex tincidunt, pellentesque tortor sit amet, rhoncus orci.'
+  });
 };
